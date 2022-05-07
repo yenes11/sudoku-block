06d82eb6812cc67cd06ed0b9e06d6cd4f3e2fa38
Enes Yurtsever <slothful_@outlook.com>
2022-05-07 23:36:56 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sudoku_block',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
